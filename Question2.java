c2d0b67fb2ae99c9ff3039c2d47603d42c69307b
5Anisa <71705614+5Anisa@users.noreply.github.com>
2020-10-13 14:54:21 +0200

    Add files via upload

---
+++ Question2.java
@@ -0,0 +1,14 @@
+package Day_4;
+
+public class Question2 {
+
+	public static void main(String[] args) {
+		
+		String name="Ozzy";
+		int age=15;
+		
+		System.out.println("\"Hello, " + name + "! " + " Based on your input, your age is " + age + ":)\"" );
+		
+	}
+
+}
