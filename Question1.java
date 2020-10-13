63c15d2cd7bc7d4f642c7ae1b4f161242b2ccb2d
5Anisa <71705614+5Anisa@users.noreply.github.com>
2020-10-13 14:53:42 +0200

    Add files via upload

---
+++ Question1.java
@@ -0,0 +1,15 @@
+package Day_4;
+
+public class Question1 {
+
+	public static void main(String[] args) {
+		
+		double gallon,liter;
+		gallon=15;
+		
+		liter=gallon*3.785411784;
+		
+		System.out.println(gallon + " gallon is " + liter + " liter ");
+	}
+
+}
